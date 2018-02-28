fe1316b143c288abfce86a4d845f5669bf35729e
cr0wst <steve@smcrow.net>
2018-02-27 19:10:16 -0500

    Initial code from spring initializr.

---
+++ src/main/java/net/smcrow/demo/twofactor/TwofactorApplication.java
@@ -0,0 +1,12 @@
+package net.smcrow.demo.twofactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwofactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwofactorApplication.class, args);
+	}
+}
