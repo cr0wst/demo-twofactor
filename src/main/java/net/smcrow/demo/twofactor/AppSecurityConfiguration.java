70219888cdc80cfde73afae2641a750ae61a000b
cr0wst <steve@smcrow.net>
2018-02-28 17:05:51 -0500

    Link up custom user details service with the dao authentication provider.

--- src/main/java/net/smcrow/demo/twofactor/AppSecurityConfiguration.java
+++ src/main/java/net/smcrow/demo/twofactor/AppSecurityConfiguration.java
@@ -1,7 +1,10 @@
 package net.smcrow.demo.twofactor;
 
+import net.smcrow.demo.twofactor.user.StandardUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +15,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    StandardUserDetailService standardUserDetailService;
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // Webjar resources
@@ -22,10 +29,17 @@ public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
-                .inMemoryAuthentication()
-                .withUser("demo")
-                .password("demo")
-                .roles("USER");
+        authenticationManagerBuilder.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        // IMPORTANT: Not setting a password encoder here means that passwords will be stored in plain text.
+        // This is _not secure_ but is being used for the sake of the demo.  Please read up on properly creating
+        // a secure authentication provider before releasing to production.
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(standardUserDetailService);
+
+        return daoAuthenticationProvider;
     }
 }
